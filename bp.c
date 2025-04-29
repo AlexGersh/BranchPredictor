1ed2075e94e6f245cf614e5d75bd1688b32f784d
TimorKoulikov <timormat321@gmail.com>
2025-04-29 13:15:30 +0300

    writing BP_predict

--- bp.c
+++ bp.c
@@ -52,6 +52,11 @@ bool isHistSizeValid(unsigned hist_size);
 bool isTagSizeValid(unsigned tag_size, unsigned btb_size);
 bool isFsmInitStateValid(unsigned fsm_init_state);
 bool isSharedValid(int shared);
+bool isPCValid(uint32_t);
+uint32_t getIProwFromPC(uint32_t);
+uint32_t getTagFromPC(uint32_t);
+void setBtbRow(Btb_row_t*,uint32_t tag,uint32_t target,uint8_t history,fsm_p fsm_pointer);
+
 
 // not finished yet
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
@@ -75,14 +80,14 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
 
     my_btb.table = (Btb_row_t *)malloc(my_btb.size * sizeof(Btb_row_t));
     if (!my_btb.table) {
-        fprintf(stderr, "faild to malloc size of table");
+        fprintf(stderr, "faild to malloc size of table\n");
         return -1; // allocation error
     }
 
     my_btb.predictor_table =
         (fsm_p)malloc((isGlobalHist ? 1 : my_btb.size) * predictor_array_size);
     if (!my_btb.predictor_table) {
-        fprintf(stderr, "faild to malloc size of table");
+        fprintf(stderr, "faild to malloc size of table\n");
         return -1; // allocation error
     }
     for (int i = 0; i < sizeof(my_btb.predictor_table); i++) {
@@ -90,20 +95,46 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
     }
 
     for (int i = 0; i < my_btb.size; i++) {
-        my_btb.table[i].tag = NEW;
-        my_btb.table[i].target = 0;
-        my_btb.table[i].history = 0;
-        // if global history all fsm_pointer points to same table
-        my_btb.table[i].fsm_pointer =
+        
+        fsm_p fsm_pointer=
             my_btb.predictor_table +
             (isGlobalHist ? 0 : i * predictor_array_size);
+        setBtbRow(&my_btb.table[i],NEW,0,0, fsm_pointer);
     }
 
     return 0; // success
 }
 
 // not finished yet
-bool BP_predict(uint32_t pc, uint32_t *dst) { return false; }
+bool BP_predict(uint32_t pc, uint32_t *dst)
+{
+    bool decision=false;
+    if(!isPCValid(pc))
+    {
+        fprintf(stderr, "PC is not valid\n");
+    }
+    *dst=pc+4;
+    uint32_t ip=getIProwByPC(pc);
+    uint32_t tag=getTagFromPC(pc);
+    Btb_row_t* row=&my_btb.table[ip];
+    //taking new branch
+    if(my_btb.table[ip].tag==NEW)
+    {
+        setBtbRow(row,tag,0
+            ,my_btb.fsm_init_st,row->fsm_pointer);
+
+    }
+    //branch in btb
+    else
+    {
+        //calculating decision
+        decision=row->fsm_pointer[row->history];
+        dst=row->target;
+
+    }
+
+    return decision;
+} 
 
 // not finished yet
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst) {
